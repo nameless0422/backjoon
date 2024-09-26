503bbc8cdba7a839fe4c3f621688501b2c3e0ca6
휴우。 <46670103+nameless0422@users.noreply.github.com>
2024-09-26 23:08:40 +0900

    Create 1011.c

---
+++ 1011.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+    int T;
+    unsigned int x, y;
+    scanf("%d", &T);
+    unsigned int sub;
+    unsigned int n1;
+    unsigned int n2;
+    int j;
+ 
+    for (int i = 0; i < T; i++) {
+        scanf("%u %u", &x, &y);
+        sub = y - x;
+        n1 = 0; n2 = 0; j = 0;
+        while (n1 < sub) {
+            n2 += j * 2;
+            n1 += ++j * 2;
+        }
+        printf("%d\n", n1 - sub < j ? j * 2 : j * 2 - 1);
+    }
+    return 0;
+}
